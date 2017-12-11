e485c9e8856548ace9dc394886a4b0eedbe91a35
ohwoontaek <dnsxor120@naver.com>
2017-12-11 15:59:29 +0900

    Java Hello 프로그램 작성

---
+++ java/Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+	// main: method(메서드),프로그램의 시작점
+	// string[] args: argument, parameter
+	public static void main(String[]	args){
+		
+		
+		//System.out.println 메서드: 괄호 안의 글자를 출력
+			System.out.println("Hello, World");
+	}
+	
+	
+}
