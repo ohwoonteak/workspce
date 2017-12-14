4ba3121ad2dea624121b10e7cd5e23ef83bb0b12
ohwoontaek <dnsxor120@naver.com>
2017-12-14 11:56:23 +0900

    반복문

---
+++ Day04/Repeat.java
@@ -0,0 +1,18 @@
+public class Repeat{
+	
+		public static void main(String[] args) {
+			
+			for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World");
+			
+			}
+			
+			for (int i = 0; i < args.length; i++) {
+					System.out.println(args[i]);
+				
+				
+			}
+		} 
+	
+	
+}
