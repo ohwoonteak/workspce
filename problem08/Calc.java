28fcf6b2438548323d155a491abb3e42659f3f0a
ohwoontaek <dnsxor120@naver.com>
2017-12-15 14:51:21 +0900

    객체추가

---
+++ problem08/Calc.java
@@ -0,0 +1,32 @@
+class Calc{
+	
+
+
+
+	
+	//int 숫자로 적용하게만든다.
+	// add 더하기 sub 빼기
+	// mul 곱하기 div 나누기
+	//메서드
+	int add(int a, int b){
+		int aaa = a + b;
+		return aaa;
+	}
+	//메서드
+	// int sub(000, 000) 000에들어가는것은 임의의 변수
+	int sub(int a, int b){
+		int aaa = a - b;
+		return aaa;
+	}
+	//메서드
+	// return은 출력하는부분이다.
+	int mul(int a, int b){
+        int aaa = a * b;
+		return aaa;
+	}
+	//메서드
+	int div(int a, int b){
+		int aaa = a / b;
+		return aaa;
+	}
+}
---
+++ problem08/TestCalc.java
@@ -0,0 +1,53 @@
+public class TestCalc{
+	
+	public static void main(String[] args){
+		
+		//객체
+		Calc aaa = new Calc(); //Calc 클래스의 객체(Object)(aaa는 변수)
+		
+		
+		
+		
+		
+		int ada = aaa.add(5, 7); //aaa.add를 int ada에 대입을한다
+		System.out.println("a와 b를 더한다");
+		// 결과를 출력한다.()안에들어가는게 출력값이다
+		
+		
+		
+		int adc = aaa.sub(4, 8);
+		System.out.println(adc);
+		
+		
+		int add = aaa.mul(10, 2);
+		System.out.println(add);
+		
+		int ade =  aaa.div(10, 10);
+		System.out.println(ade);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
