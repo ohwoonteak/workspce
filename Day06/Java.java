3a27af546882d14c2c07db0ef372346a1c9c9b2c
ohwoontaek <dnsxor120@naver.com>
2017-12-18 16:46:54 +0900

    20171218수정

---
+++ Day06/Java.java
@@ -0,0 +1,37 @@
+public class Java{
+	public static void main(String[] args){
+		
+		
+		//'H'를 출력
+		for (char i = 65; i < 91; i++){
+			if(i == 'H'){
+				System.out.print(i);
+				break;
+			}
+		}
+		//'E'를 출력
+		for (char i = 65; i < 91; i++){
+			if(i == 'E'){
+				System.out.print(i);
+				break;
+			}
+		}
+		//'L'을 출력
+		for (char i = 65; i < 91; i++){
+			if(i == 'L'){
+				System.out.print(i);
+				System.out.print(i);
+				break;
+			}
+		}
+	
+		//'O'를 출력
+		for (char i = 65; i < 91; i++){
+			if(i == 'O'){
+				System.out.print(i);
+				break;
+			}
+		}
+	}
+	//A~Z까지 읽다가 조건에맞으면 종료가된다.
+}
